c0191069a82f8faf130159983f6b4676ed54da6c
상원 <ga8457@naver.com>
2019-11-12 11:29:37 +0900

    11/12

--- app/src/main/java/com/example/beacontest/MainActivity.java
+++ app/src/main/java/com/example/beacontest/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.beacontest;
 
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.RemoteException;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -21,7 +28,7 @@ import org.altbeacon.beacon.Region;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.UUID;
+
 
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
@@ -33,8 +40,6 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
     // 감지된 비콘들을 임시로 담을 리스트
     private List<Beacon> beaconList = new ArrayList<>();
     TextView textView;
-    TextView count;
-    TextView count2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,37 +51,53 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
 
         beaconManager = BeaconManager.getInstanceForApplication(this);
         textView = (TextView) findViewById(R.id.textView);
-        count = (TextView) findViewById(R.id.count);
-        count2 = (TextView)findViewById(R.id.count2);
+        Button button = (Button)findViewById(R.id.button);
 
-        textView.setText("연동 대기중...");
-        count2.setText("\n연동 대기중...");
         beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
 
         // 비콘 탐지를 시도한다
-        beaconManager.bind(this);
+        beaconManager.bind(MainActivity.this);
         handler.sendEmptyMessage(0);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("주변에 비콘을 탐색 중입니다...");
+                findViewById(R.id.button).setVisibility(View.INVISIBLE);
+                beaconManager.bind(MainActivity.this);
+                handler.sendEmptyMessage(0);
+            }
+        });
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        //계속 메모리에 상주하고 있을 수 있으므로 unbind 시켜야함
-        beaconManager.unbind(this);
     }
 
     @Override
-    public void onBeaconServiceConnect() {
-        beaconManager.setRangeNotifier(new RangeNotifier() {
-            @Override
-            // 비콘이 감지되면 해당 함수가 호출된다. Collection<Beacon> beacons에는 감지된 비콘의 리스트가,
-            // region에는 비콘들에 대응하는 Region 객체가 들어온다.
-            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-                if (beacons.size() > 0) {
-                    for (Beacon beacon : beacons) {
-                        beaconList.add(beacon);
-                    }
-                }
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        textView.setText("기존 비콘이 삭제되었습니다. \n재연동 버튼을 눌러주세요");
+    }
+
+    @Override
+                public void onBeaconServiceConnect() {
+                    beaconManager.setRangeNotifier(new RangeNotifier() {
+                        @Override
+                        // 비콘이 감지되면 해당 함수가 호출된다. Collection<Beacon> beacons에는 감지된 비콘의 리스트가,
+                        // region에는 비콘들에 대응하는 Region 객체가 들어온다.
+                        public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                            if (beacons.size() > 0) {
+                                for (Beacon beacon : beacons) {
+                                    beaconList.add(beacon);
+                                }
+                            }
             }
 
         });
@@ -92,30 +113,42 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
         }
     }
 
+    @SuppressLint("HandlerLeak")
     Handler handler = new Handler() {
         public void handleMessage(Message msg) {
 
-            for (Beacon beacon:beaconList){
-                if (beacon.getBluetoothAddress().equals("D0:B5:C2:C7:90:47")) {
+                for (Beacon beacon:beaconList) {
                     String beaconinfo = "UUID: " + beacon.getId1() + " \nMajor: " + beacon.getId2() + "\nMinor: " + beacon.getId3() + " \n비콘과의 떨어진 거리: " + Double.parseDouble(String.format("%.3f", beacon.getDistance())) + "m\n"
                             + "연동된 비콘이름: " + beacon.getBluetoothName() + " \n맥주소: " + beacon.getBluetoothAddress() + "\n신호량: " + beacon.getRssi();
 
-                    Log.d("beaconInfo", beacon.getBluetoothAddress());
+
+                    if (beacon.getDistance() > 1.0) {
+                        Toast.makeText(MainActivity.this, "비콘이 주변에 있습니다(1m) Daum으로 이동", Toast.LENGTH_SHORT).show();
+                        daum_handler.sendEmptyMessage(1);
+                        beaconManager.unbind(MainActivity.this);
+                    } else {
+                        Log.d("Distance", "1m 이상");
+                    }
+
+                    Log.d("beaconInfo", beacon.toString());
                     textView.setText(beaconinfo);
-
-                } else {
-                    String beaconinfo = "UUID: " + beacon.getId1() + " \nMajor: " + beacon.getId2() + "\nMinor: " + beacon.getId3() + " \n비콘과의 떨어진 거리: " + Double.parseDouble(String.format("%.3f", beacon.getDistance())) + "m\n"
-                            + "연동된 비콘이름: " + beacon.getBluetoothName() + " \n맥주소: " + beacon.getBluetoothAddress() + "\n신호량: " + beacon.getRssi();
-
-                    Log.d("beaconInfo2", beacon.toString());
-                    count2.setText(beaconinfo);
                 }
-            }
-
-
             handler.sendEmptyMessageDelayed(0, 1000);
         }
     };
 
+    Handler daum_handler = new Handler(){
+        public void handleMessage(Message msg) {
+           try {
+               beaconManager.unbind(MainActivity.this);
+               beaconList.clear();
+           } catch(Exception e) {
+               e.printStackTrace();
+           }
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.daum.net"));
+            startActivity(intent);
+            findViewById(R.id.button).setVisibility(View.VISIBLE);
+        }
+    };
 
 }
